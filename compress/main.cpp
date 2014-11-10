b14cc6a7e4c3d223cc09b9b6fa7f15f245a0773b
Robinde67 <a>
2014-11-10 10:39:52 +0100

    initial commit Xcode

---
+++ compress/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  compress
+//
+//  Created by Robin on 10/11/14.
+//  Copyright (c) 2014 FRAD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
